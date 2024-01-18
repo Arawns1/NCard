b233f975327dcee3507ee791d7c4cc8cb97da326
Gabriel Damico <gabrieldamico22@gmail.com>
2024-01-18 11:40:35 -0300

    🎉 Commit inicial - Configurações Básicas
    Configuração:
    - Docker Compose
    - Application Properties
    - APP-dev properties

---
+++ server/src/main/java/br/com/itneki/nekicard/NekicardApplication.java
@@ -0,0 +1,13 @@
+package br.com.itneki.nekicard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NekicardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NekicardApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/br/com/itneki/nekicard/NekicardApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.itneki.nekicard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NekicardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
