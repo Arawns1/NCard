c6159b298159573bc0d77a09ce504ecb4eca7717
Gabriel Damico <gabrieldamico22@gmail.com>
2024-01-19 08:48:36 -0300

    docs: Added import mock data and banner

--- server/src/main/java/br/com/itneki/nekicard/config/SwaggerConfig.java
+++ server/src/main/java/br/com/itneki/nekicard/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package br.com.itneki.nekicard.config;
 
+import io.swagger.v3.oas.models.info.Contact;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.OpenAPI;
@@ -12,7 +13,12 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .info(new Info().title("NekiCard API Documentation")
                                 .description("Official documentation of NekiCard API Aplication")
-                                .version("1.0.0"))
+                                .version("1.0.0")
+                                .contact(new Contact()
+                                                .email("gabrieldamico22@gmail.com")
+                                                .name("Gabriel Damico")
+                                                .url("https://github.com/Arawns1")
+                                        ))
                 .schemaRequirement("jwt_auth", createSecurityScheme());
     }
     private SecurityScheme createSecurityScheme(){
