fb0cd96215b260962caba5968905304fe8dcf623
Gabriel Damico <gabrieldamico22@gmail.com>
2024-01-23 15:05:57 -0300

    feat(Web): UserScreen completed

--- web/nekicard/src/components/ContactCard.tsx
+++ web/nekicard/src/components/ContactCard.tsx
@@ -1,10 +1,14 @@
 import React, { ReactElement, cloneElement } from 'react'
+import { useToast } from '@/components/ui/use-toast'
 
 interface ContactCardProps {
   icon: ReactElement
   text: string
   variant?: 'default' | 'horizontal'
+  link?: string
+  canCopy?: boolean
 }
+
 export default function ContactCard({
   variant = 'default',
   ...rest
@@ -19,29 +23,58 @@ export default function ContactCard({
   }
 }
 
-export function DefaultContactCard({ icon, text }: ContactCardProps) {
-  return (
-    <div className="md:h-28 md:w-28 w-24 h-20 bg-gray-500 flex  flex-col items-center justify-center rounded-xl overflow-hidden space-y-2 hover:cursor-pointer hover:opacity-[0.8]">
-      {icon && cloneElement(icon, { className: 'w-8 h-8 text-gray-200' })}
+export function DefaultContactCard({
+  icon,
+  text,
+  link,
+  canCopy,
+}: ContactCardProps) {
+  const { toast } = useToast()
 
-      <span className="font-normal text-gray-200">{text}</span>
-    </div>
+  const handleCopy = () => {
+    if (link) {
+      navigator.clipboard.writeText(link)
+      toast({
+        title: '🔗 Link copiado!',
+        duration: 2000,
+        style: {
+          backgroundColor: '#323238',
+          borderRadius: '8px',
+        },
+      })
+    }
+  }
+
+  return (
+    <a
+      href={canCopy ? undefined : link}
+      target="_blank"
+      onClick={canCopy ? handleCopy : undefined}
+    >
+      <div className="md:h-28 md:w-28 w-24 h-20 bg-gray-500 flex  flex-col items-center justify-center rounded-xl overflow-hidden space-y-2 hover:cursor-pointer hover:opacity-[0.8]">
+        {icon && cloneElement(icon, { className: 'w-8 h-8 text-gray-200' })}
+
+        <span className="font-normal text-gray-200">{text}</span>
+      </div>
+    </a>
   )
 }
 
-export function HorizontalContactCard({ icon, text }: ContactCardProps) {
+export function HorizontalContactCard({ icon, text, link }: ContactCardProps) {
   return (
-    <div className="md:h-12 md:w-28 w-24 h-10 bg-gray-500 flex flex-row items-center justify-center rounded-xl space-x-2 overflow-hidden space-y-2 hover:cursor-pointer hover:opacity-[0.8]">
-      {icon &&
-        cloneElement(icon, {
-          className: 'sm: w-6 sm: h-6 w-4 h-4 text-gray-200 ',
-        })}
+    <a href={link} target="_blank">
+      <div className="md:h-12 md:w-28 w-24 h-10 bg-gray-500 flex flex-row items-center justify-center rounded-xl space-x-2 overflow-hidden space-y-2 hover:cursor-pointer hover:opacity-[0.8]">
+        {icon &&
+          cloneElement(icon, {
+            className: 'sm: w-6 sm: h-6 w-4 h-4 text-gray-200 ',
+          })}
 
-      <div className="h-full flex items sm:py-[10px] py-[6px]">
-        <span className="  md:font-normal text-sm font-thin text-gray-200 ">
-          {text}
-        </span>
+        <div className="h-full flex items sm:py-[10px] py-[6px]">
+          <span className="  md:font-normal text-sm font-thin text-gray-200 ">
+            {text}
+          </span>
+        </div>
       </div>
-    </div>
+    </a>
   )
 }
--- web/nekicard/src/pages/UserPage.tsx
+++ web/nekicard/src/pages/UserPage.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react'
-import bgHeader from '@/assets/HeaderBgBlack.png'
 import defaultUserPhoto from '@/assets/userPhotoDefault@2x.png'
-
+import HeaderBgBlue from '@/assets/HeaderBgBlue.png'
+import HeaderBgDarkBlue from '@/assets/HeaderBgDarkBlue.png'
+import HeaderBgBlack from '@/assets/HeaderBgBlack.png'
 import { api } from '@/services/axios'
 import { UserProfileDTO } from '@/dtos/UserProfile'
 import { Avatar, AvatarImage } from '@/components/ui/avatar'
@@ -11,12 +12,15 @@ import GithubIcon from '@/assets/githubIcon.svg'
 import FacebookIcon from '@/assets/facebookIcon.svg'
 import LinkedinIcon from '@/assets/linkedinIcon.svg'
 import { useParams } from 'react-router-dom'
+import { Toaster } from '@/components/ui/toaster'
 export default function UserPage() {
   const { userId } = useParams()
   const [user, setUser] = useState<UserProfileDTO>()
+  const [headerPhoto, setHeaderPhoto] = useState(HeaderBgBlack)
 
   useEffect(() => {
     fetchUser()
+    fetchHeaderPhoto()
   }, [])
 
   const findLinkedin = user?.socialMediaList?.find(
@@ -34,14 +38,27 @@ export default function UserPage() {
     setUser(response.data)
     return response.data
   }
+  const fetchHeaderPhoto = async () => {
+    const type = user?.cardList?.at(0)?.type || 'BLUE'
+    switch (type) {
+      case 'DARK_BLUE':
+        setHeaderPhoto(HeaderBgDarkBlue)
+        break
+      case 'BLACK':
+        setHeaderPhoto(HeaderBgBlack)
+        break
+      default:
+        setHeaderPhoto(HeaderBgBlue)
+    }
+  }
 
   return (
-    <div className="w-full h-full flex flex-col bg-gray-700 items-center justify-center">
+    <div className="w-full h-full flex flex-col bg-gray-700 items-center justify-center pb-10">
       <div className="w-full h-80">
-        <img src={bgHeader} className="w-full h-full object-cover " />
+        <img src={headerPhoto} className="w-full h-full object-cover " />
       </div>
       <main className="flex flex-col items-center justify-center space-y-6 mt-[-65px] md:mt-[-90px]">
-        <Avatar className="w-32 h-32 md:w-44 md:h-44">
+        <Avatar className="w-32 h-32 md:w-44 md:h-44 border-zinc-900 border-4">
           <AvatarImage src={user?.profilePhotoUrl || defaultUserPhoto} />
         </Avatar>
         <div className="flex flex-col items-center justify-center">
@@ -60,61 +77,66 @@ export default function UserPage() {
           </div>
         )}
 
-        <div id="myContacts" className="w-full space-y-3">
+        <div id="myContacts" className="w-full space-y-3 ">
           <h2 className="text-xl font-bold tx-gray-100">Meus Contatos</h2>
-          <div className=" flex flex-row items-start justify-between  sm:px-12 px-1">
+          <div className=" flex flex-row items-start justify-between   px-1">
             <ContactCard
+              link={`${window.location.origin.toString()}/user/${userId}`}
               text="Meu Link"
+              canCopy
               icon={<LinkSimple weight="regular" />}
             />
             <ContactCard
+              link={`mailto:${user?.email}`}
               text="Email"
-              icon={
-                <Envelope weight="regular" href={`mailto:${user?.email}`} />
-              }
+              icon={<Envelope weight="regular" />}
             />
-            <ContactCard text="Celular" icon={<Phone weight="regular" />} />
+            <ContactCard
+              link={`tel:${user?.phone}`}
+              text="Celular"
+              icon={<Phone weight="regular" />}
+            />
           </div>
         </div>
+        {findLinkedin || findFacebook || findGithub ? (
+          <div id="mySocialMedias" className="w-full space-y-3">
+            <h2 className="text-xl font-bold tx-gray-100">
+              Minhas Redes Sociais
+            </h2>
+            <div className="flex flex-row items-start justify-between   px-1 ">
+              {findGithub ? (
+                <ContactCard
+                  variant="horizontal"
+                  text="Github"
+                  link={findGithub}
+                  icon={<img src={GithubIcon} />}
+                />
+              ) : null}
 
-        {findLinkedin ||
-          findFacebook ||
-          (findGithub && (
-            <div id="mySocialMedias" className="w-full space-y-3">
-              <h2 className="text-xl font-bold tx-gray-100">
-                Minhas Redes Sociais
-              </h2>
-              <div className="flex flex-row items-start justify-between  sm:px-12 px-1">
-                {findGithub && (
-                  <ContactCard
-                    variant="horizontal"
-                    text="Github"
-                    icon={<img src={GithubIcon} />}
-                  />
-                )}
+              {findLinkedin ? (
+                <ContactCard
+                  variant="horizontal"
+                  text="Linkedin"
+                  link={findLinkedin}
+                  icon={<img src={LinkedinIcon} />}
+                />
+              ) : null}
 
-                {findLinkedin && (
-                  <ContactCard
-                    variant="horizontal"
-                    text="Linkedin"
-                    icon={<img src={LinkedinIcon} />}
-                  />
-                )}
-
-                {findFacebook && (
-                  <ContactCard
-                    variant="horizontal"
-                    text="Facebook"
-                    icon={<img src={FacebookIcon} />}
-                  />
-                )}
-              </div>
+              {findFacebook ? (
+                <ContactCard
+                  variant="horizontal"
+                  text="Facebook"
+                  link={findFacebook}
+                  icon={<img src={FacebookIcon} />}
+                />
+              ) : null}
             </div>
-          ))}
+          </div>
+        ) : null}
 
         <div id="Details" className="w-full space-y-2">
           <h2 className="text-xl font-bold">Detalhes</h2>
-          <ul className="list-none">
+          <ul className="list-none flex flex-col items-start">
             <li>
               <span className="font-bold text-muted-foreground">
                 Nome social:{' '}
@@ -122,18 +144,26 @@ export default function UserPage() {
               <span className="font-normal text-muted-foreground">
                 {user?.socialName}
               </span>
+            </li>
+            <li>
               <span className="font-bold text-muted-foreground">
                 Data de Nascimento:{' '}
               </span>
               <span className="font-normal text-muted-foreground">
                 {user?.birthdate}
               </span>
+            </li>
+
+            <li>
+              {' '}
               <span className="font-bold text-muted-foreground">
                 Localidade:{' '}
               </span>
               <span className="font-normal text-muted-foreground">
                 {user?.locality}
               </span>
+            </li>
+            <li>
               <span className="font-bold text-muted-foreground">
                 Tempo de Neki:{' '}
               </span>
@@ -144,6 +174,7 @@ export default function UserPage() {
           </ul>
         </div>
       </main>
+      <Toaster />
     </div>
   )
 }
